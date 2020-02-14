9e50e892d7d2fd94d08c6234a3175cf11a290e52
gordejka <gordeu1993@gmail.com>
2020-02-14 14:55:02 +0300

    3.1 - create state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import Car from './Car/Car';
+import React, { Component } from "react";
+import "./App.css";
+import Car from "./Car/Car";
 
-class App extends Component{
-  render(){
-    const styles = {
-      fontSize:'50px'
+class App extends Component {
+    state = {
+      cars: [
+        {name:"Ford", year:2018},
+        {name:"Bmw", year:2016},
+        {name:"Audi", year:2012}
+      ],
+      pageTitle:'React components'
+    };
+    render() {
+        const styles = {
+            fontSize: "50px"
+        };
+        const cars = this.state.cars;
+        return (
+            <div>
+                <h1 style={styles}>{this.state.pageTitle}</h1>
+                <Car name={cars[0].name} year={cars[0].year} />
+                <Car name={cars[1].name} year={cars[1].year} />
+                <Car name={cars[2].name} year={cars[2].year} />
+            </div>
+        );
     }
-    return (
-      <div>
-        <h1 style={styles}>hello world</h1>
-        <Car name={'Ford'} year={2019}>
-          <p>123</p>
-        </Car>
-        <Car name={'Bmw'} year={2016}/>
-      </div>
-    );
-  }
 }
 
 export default App;
