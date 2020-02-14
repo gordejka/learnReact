ab88c5f19bdda3c70ffc632db183731bc79be3a8
gordejka <gordeu1993@gmail.com>
2020-02-14 15:21:09 +0300

    3.3 - change state with setState()

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import Car from "./Car/Car";
 
 class App extends Component {
     state = {
-      cars: [
-        {name:"Ford", year:2018},
-        {name:"Bmw", year:2016},
-        {name:"Audi", year:2012}
-      ],
-      pageTitle:'React components'
+        cars: [
+            { name: "Ford", year: 2018 },
+            { name: "Bmw", year: 2016 },
+            { name: "Audi", year: 2012 }
+        ],
+        pageTitle: "React components"
     };
 
-    changeTitleHandler = () =>{
-      console.log('clicked');
-    }
+    changeTitleHandler = () => {
+        const oldTitle = this.state.pageTitle;
+        const newTitle = oldTitle + "(changed)";
+        this.setState({
+            pageTitle: newTitle
+        });
+    };
 
     render() {
-        const styles = {
-            fontSize: "50px"
-        };
+        console.log("render");
         const cars = this.state.cars;
         return (
             <div>
-                <h1 style={styles}>{this.state.pageTitle}</h1>
-
-                <button onClick={this.changeTitleHandler}>Change title</button> 
-
+                <h1>{this.state.pageTitle}</h1>
+                <button onClick={this.changeTitleHandler}>Change title</button>
                 <Car name={cars[0].name} year={cars[0].year} />
                 <Car name={cars[1].name} year={cars[1].year} />
                 <Car name={cars[2].name} year={cars[2].year} />
